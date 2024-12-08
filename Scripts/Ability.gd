ae24b36f50b5a8d6ac86107d0428a8dfa6017014
jgschuma <47636951+jgschuma@users.noreply.github.com>
2024-12-08 04:19:50 -0500

    Ability and passive framework
    We now have a model for how to make working abilities and passives

--- Scripts/Ability.gd
+++ Scripts/Ability.gd
@@ -2,9 +2,8 @@ extends Resource
 class_name Ability
 
 @export var abilityName: String
-@export var abilityEffect: Script
 @export var abilityCost: int
 
-func onEnergyMet():
+func onEnergyMet(user: HeroScene):
 	#abilityEffect.cast()
-	print("This is where my cast function call would go")
+	pass
